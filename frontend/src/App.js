23d63c0a021692325387aa2fcf53db5e9f8c81de
Sai Ramana Reddy <sairamana@tamu.edu>
2023-11-09 16:08:07 +0000

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Visualization from './Visualization';
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Visualization /> {/* Render the Visualization component here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
