500f4871e8694451c3c5836234e5889d8eabd614
Millennium <millennium.milan1@gmail.com>
2023-11-24 06:21:43 +0000

    Multiline plot with Sector filter

--- backend/main.py
+++ backend/main.py
@@ -7,8 +7,8 @@ import pandas as pd
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
-    "localhost:3000"
+    "http://localhost:3002",
+    "localhost:3002"
 ]
 
 
@@ -19,6 +19,35 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+'''
+Statistics:
+--------------------------------
+H-1B               5038050
+H-2A                105401
+E-3 Australian       83874
+H-2B                 36137
+H-1B1 Singapore       8009
+H-1B1 Chile           6430
+--------------------------------=
+Professional and business services                                   4265613
+Educational services, health care, and social assistance              353937
+Finance, insurance, real estate, rental, and leasing                  237660
+Others                                                                185207
+Agriculture, forestry, fishing, and hunting                           107226
+Arts, entertainment, recreation, accommodation, and food services      67984
+Information                                                            26184
+Utilities                                                              18410
+Manufacturing                                                           3810
+Government                                                              3534
+Construction                                                            3235
+Transportation and Warehousing                                          2222
+Wholesale and Retail Trade                                              2129
+Real estate and rental and leasing                                       606
+Mining                                                                   144
+-------------------------------
+
+'''
 
 class FilterStates(BaseModel):
     selectedFiltersArr: List[List[str]]
@@ -55,16 +84,23 @@ class StackedBarData(BaseModel):
 }
 '''
 
+gdp_data = pd.read_csv("data/gdp_2010_2020.csv")
 
 @app.post("/api/lineplot", tags=["lineplot"])
 async def lineplot(filterStates: FilterStates) -> LineChartData:
-    print(filterStates)
-    df = pd.read_csv("data/gdp_2010_2020.csv")
-    xArr = df.columns[1:].tolist()
+    print("filterStates:", filterStates)
+
+    selectedSectors = ['All Sector'] ### By default it will be All Sector
+    if len(filterStates.selectedFiltersArr[2]) > 0:
+        selectedSectors = filterStates.selectedFiltersArr[2]
+
+    temp_gdp_df = gdp_data[gdp_data['Sector'].isin(selectedSectors)]
+    print(temp_gdp_df)
+    xArr = temp_gdp_df.columns[1:].tolist()
     yArr = []
-    for i in range(len(df)):
-        yArr.append(df.iloc[i, 1:].astype(float).tolist())
-    yLabelArr = df.iloc[:, 0].tolist()
+    for i in range(len(temp_gdp_df)):
+        yArr.append(temp_gdp_df.iloc[i, 1:].astype(float).tolist())
+    yLabelArr = temp_gdp_df.iloc[:, 0].tolist()
     return LineChartData(labels=xArr, datasets=[LineData(label=yLabelArr[i], data=yArr[i]) for i in range(len(yArr))])
 
 
